c1200a871f89424899c1e0f1eaa18dda7828a208
박영철 <parkdigy@gmail.com>
2023-09-05 11:29:40 +0900

    1.0.1

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:true});var react=require('react');function useFirstSkipEffect(effect, deps) {
+'use strict';var react=require('react');function useFirstSkipEffect(effect, deps) {
     var firstRef = react.useRef(true);
     react.useEffect(function () {
         if (firstRef.current) {
--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -9,6 +9,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const { SourceMapDevToolPlugin } = require('webpack');
 
 //--------------------------------------------------------------------------------------------------------------------
 
@@ -105,6 +106,9 @@ const options = {
       : {},
   },
   plugins: [
+    new SourceMapDevToolPlugin({
+      filename: '[file].map',
+    }),
     new WebpackShellPluginNext({
       dev: !isProduction,
       onBuildStart: {
@@ -163,6 +167,11 @@ const options = {
             },
           },
         ],
+      },
+      {
+        test: /\.m?js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
       },
       {
         test: /\.html$/,
